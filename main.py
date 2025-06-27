fb25b865e9a61201da2f04a70a2ec2faec6c86e8
Nico-Sander <sandernico512@gmail.com>
2025-06-27 23:15:00 +0200

    Finished course 'Build an AI Agent in Python

--- main.py
+++ main.py
@@ -33,7 +33,21 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages, verbose)
+    iters = 0
+    while True:
+        iters += 1
+        if iters > 20:
+            print(f"Maximum iterations (20) reached.") 
+            sys.exit(1)
+
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
 
 def generate_content(client, messages, verbose):
@@ -48,19 +62,32 @@ def generate_content(client, messages, verbose):
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
 
+    # Check candidates
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
+
     if not response.function_calls:
-        return response.text
-
+        # No function call -> return final response.
+        return response.text    
+    
+    function_responses = []
     for function_call_part in response.function_calls:
-        # print(f"Calling function: {function_call_part.name}({function_call_part.args})"
-        function_call_result = call_function(function_call_part, verbose=True)
-
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception("Fatal: Called function didn't return a result!")
-
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
         if verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
+            print(f"-> {function_call_result.parts[0].function.response.response}")
+        function_responses.append(function_call_result.parts[0])
 
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
+    messages.append(types.Content(role="tool", parts=function_responses))
         
 
 
